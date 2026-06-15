9df21bd8a7714b9e6dd7594c9247967cc08d533c
Tomasz Karczewski <tomasz.2173644@infosys.com>
2026-06-15 16:06:40 +0200

    build volume; untested

--- devtools/scripts/flutter_dev_entrypoint.sh
+++ devtools/scripts/flutter_dev_entrypoint.sh
@@ -13,13 +13,18 @@ if [ -n "$HOST_UID" ] && [ -n "$HOST_GID" ]; then
     chown -R flutter-dev:flutter-dev /home/flutter-dev
 fi
 
+if [ -n "$BOLT_BUILD_VOLUME_NAME" ]; then
+    mkdir -p "${REPO_ROOT}/build"
+    chown flutter-dev:flutter-dev "${REPO_ROOT}/build"
+fi
+
 if [ -z "${TMUX_INIT_SCRIPT}" ]; then
     TMUX_INIT_SCRIPT="/usr/local/bin/tmux_init.sh"
 fi
 export TMUX_INIT_SCRIPT
 
 # Drop privileges to flutter-dev and start the tmux session
-exec sudo --preserve-env=REPO_ROOT,HOST_UID,HOST_GID,FLUTTER_PROJECT_SOURCE_CODE_PATH,FLUTTER_BOLT_NAME,STB_IP,FLUTTER_APPLICATION_RECIPE,SSTATE_PATH,DOWNLOADS_PATH,INIT_CACHE_PATH,TMUX_INIT_SCRIPT -H -u flutter-dev bash <<'EOF'
+exec sudo --preserve-env=REPO_ROOT,HOST_UID,HOST_GID,FLUTTER_PROJECT_SOURCE_CODE_PATH,FLUTTER_BOLT_NAME,STB_IP,FLUTTER_APPLICATION_RECIPE,SSTATE_PATH,DOWNLOADS_PATH,INIT_CACHE_PATH,TMUX_INIT_SCRIPT,BOLT_BUILD_VOLUME_NAME -H -u flutter-dev bash <<'EOF'
 tmux new-session -d -s dev "${TMUX_INIT_SCRIPT}"
 
 # Keep the container alive by polling the tmux session
